da690d50d2ab080ad2d6eb2f66a0c67ba80523ea
alifeee <alifeee.web@outlook.com>
2025-06-25 20:16:51 +0100

    add secrets file

---
+++ monitor/src/secrets.example.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
+
+#define INFLUXDB_URL "https://influxdb.subdomain.uk"
+#define INFLUXDB_TOKEN "uaiwhd78h78h78h78dh783q2hd78h2qy78qf98qu98fh8q9u8u893qu8f3"
+#define INFLUXDB_ORG "72yd827d827y728y"
+#define INFLUXDB_BUCKET "bucketname"
